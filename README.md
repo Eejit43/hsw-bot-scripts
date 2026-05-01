6741171161395a9a048f6933f650f233d077e60d
Will <76887639+Eejit43@users.noreply.github.com>
2026-05-01 16:10:50 -0400

    feat: init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# hsw-bot-scripts
+
+This repository contains some bot scripts used in maintaining the [Hypixel SkyBlock Wiki](https://hypixelskyblock.minecraft.wiki).
---
+++ index.ts
@@ -0,0 +1,63 @@
+/* eslint-disable no-await-in-loop, unicorn/no-process-exit */
+
+import { Mwn } from 'mwn';
+import { readdirSync } from 'node:fs';
+import readline from 'node:readline/promises';
+import { styleText } from 'node:util';
+
+const readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readlineInterface.on('SIGINT', () => {
+    console.log();
+
+    readlineInterface.close();
+    process.exit(0);
+});
+
+const availableScripts = readdirSync('scripts').map((file) => file.replace('.ts', ''));
+
+console.log(styleText(['blue', 'underline'], 'Available scripts:'));
+
+console.log(`  ${availableScripts.join(',')}`);
+
+let loadedMwnInstance: Mwn | undefined;
+
+while (true) {
+    const input = await readlineInterface.question(styleText(['green'], '\nScript to run (or "exit"): '));
+
+    if (input === 'exit') break;
+
+    if (!availableScripts.includes(input)) {
+        console.log(styleText(['red'], 'Invalid script name. Please try again.'));
+        continue;
+    }
+
+    loadedMwnInstance ??= await Mwn.init({
+        apiUrl: 'https://hypixelskyblock.minecraft.wiki/api.php',
+        username: process.env.BOT_USERNAME,
+        password: process.env.BOT_PASSWORD,
+        defaultParams: { assert: 'user' },
+    });
+
+    let mainFunction: (mwn: Mwn) => Promise<void>;
+    try {
+        mainFunction = ((await import(`./scripts/${input}.ts?cb=${Date.now()}`)) as { default: (mwn: Mwn) => Promise<void> }).default;
+    } catch (error) {
+        console.error(styleText(['red'], `Failed to load script "${input}": ${error instanceof Error ? error.message : String(error)}`));
+        continue;
+    }
+
+    try {
+        await mainFunction(loadedMwnInstance);
+    } catch (error) {
+        console.error(styleText(['red'], `Error running script "${input}": ${error instanceof Error ? error.message : String(error)}`));
+        continue;
+    }
+
+    console.log(styleText(['green'], `Finished running script "${input}".`));
+}
+
+readlineInterface.close();
