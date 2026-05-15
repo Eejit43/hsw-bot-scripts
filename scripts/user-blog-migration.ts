6f8abdfcacef07d4a2b1e34a361faf2de7f06103
Will <76887639+Eejit43@users.noreply.github.com>
2026-05-15 17:39:08 -0400

    feat: finish user-blog-migration script

--- scripts/user-blog-migration.ts
+++ scripts/user-blog-migration.ts
@@ -1,28 +1,51 @@
 /* eslint-disable no-await-in-loop */
 
 import { Mwn } from 'mwn';
-import { getAllPagesInNamespace } from '../functions';
-
-const IS_DRY_RUN = true as boolean;
+import { getAllPagesInNamespace, getAllUsers } from '../functions';
 
 const USER_BLOG_NAMESPACE = 500;
 
 /**
- * This script is used to migrate `User blog:.../...` pages to the new `User:.../blog/...` format.
+ * This script is used to migrate `User blog:.../...` pages to the new `User:.../blogs/...` format.
  * @param mwn The Mwn instance.
  */
 export default async function main(mwn: Mwn) {
     const userBlogPages = await getAllPagesInNamespace(mwn, USER_BLOG_NAMESPACE);
 
-    let blogsMoved = 0;
+    const allUsers = new Set(await getAllUsers(mwn));
 
-    for (const blog of userBlogPages.slice(0, 1)) {
-        const destination = blog.title.replace('User blog', 'User').replace('/', '/blog/');
+    const usersWithBlogs = new Set<string>();
 
-        if (!IS_DRY_RUN) await mwn.move(blog.title, destination, 'Moving deprecated blog post to userspace', { noredirect: true });
+    const parsedBlogPages = userBlogPages.map((blogPage) => {
+        const username = blogPage.title.split('/')[0].split(':').slice(1).join(':');
+        const blogTitle = blogPage.title.split('/').slice(1).join('/');
 
-        blogsMoved++;
+        console.log({ username, blogTitle });
+
+        if (!allUsers.has(username)) throw new Error(`User ${username} does not exist on the wiki!`);
+
+        if (!usersWithBlogs.has(username)) usersWithBlogs.add(username);
+
+        const destination = `User:${username}/blogs/${blogTitle}`;
+
+        return { oldLocation: blogPage.title, newLocation: destination };
+    });
+
+    for (const blogPage of parsedBlogPages) {
+        Mwn.log(`[i] Moving ${blogPage.oldLocation} to ${blogPage.newLocation}...`);
+
+        await mwn.move(blogPage.oldLocation, blogPage.newLocation, 'Moving deprecated blog post to userspace', { noredirect: true });
     }
 
-    Mwn.log(`[S] Finished moving ${blogsMoved} blog${blogsMoved === 1 ? '' : 's'}.`);
+    for (const username of usersWithBlogs) {
+        const blogListingTitle = `User:${username}/blogs`;
+
+        const content = `{{Special:PrefixIndex/${blogListingTitle}/|stripprefix=yes}}`;
+
+        Mwn.log(`[i] Creating blog listing page ${blogListingTitle}...`);
+
+        await mwn.create(blogListingTitle, content, 'Creating blog listing page');
+    }
+
+    Mwn.log(`[S] Finished moving ${parsedBlogPages.length} blogs to userspace and creating ${usersWithBlogs.size} blog listing pages.`);
 }
